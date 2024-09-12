39e74542b2da49171c576d56f6b0c907064f392a
RahulrawK <rahulraw2002@gmail.com>
2024-09-12 23:37:29 +0530

    Render live deployed

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get('/author', (req, res) => {
 
 
 // Start the server
-const PORT = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3000;
+// const PORT = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
