388d961994f717e7c71fa08eb6d685a1908baa8a
RahulrawK <rahulraw2002@gmail.com>
2024-09-13 01:41:42 +0530

    Render live deployed

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
 
 
 module.exports.config = {
     PORT: process.env.PORT || 3000,
-    JWT_SECRET:'radsfgtshyagajgsjsh'}; 
-    mongoose.set('debug', true);
-    mongoose.connect("mongodb+srv://rahulraw2002:g9qIYXGNoc3JcQJV@taskmanagemnet.bai3b.mongodb.net/?retryWrites=true&w=majority&appName=taskmanagemnet", { serverSelectionTimeoutMS: 30000
-      })
-      .then(() => console.log('MongoDB connected'))
-      .catch(err => console.error('MongoDB connection error:', err));
+    JWT_SECRET: 'radsfgtshyagajgsjsh'
+};
+mongoose.set('debug', true);
+mongoose.connect("mongodb+srv://rahulraw2002:g9qIYXGNoc3JcQJV@taskmanagemnet.bai3b.mongodb.net/?retryWrites=true&w=majority&appName=taskmanagemnet", {
+    useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
