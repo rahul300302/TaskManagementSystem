cf6ba1b4b247eb48f85d3b6ddb445c7dd3b3053f
RahulrawK <rahulraw2002@gmail.com>
2024-09-13 01:19:25 +0530

    Render live deployed

--- db.js
+++ db.js
@@ -5,12 +5,9 @@ const mongoose = require('mongoose');
 
 module.exports.config = {
     PORT: process.env.PORT || 3000,
-    JWT_SECRET:'radsfgtshyagajgsjsh',
-    MONGODB_URI: 'mongodb+srv://rahulraw2002:rahulraw2002@cluster0.mongodb.net/taskManagement?retryWrites=true&w=majority'}; 
-
-    mongoose.connect("mongodb+srv://rahulraw2002:g9qIYXGNoc3JcQJV@taskmanagemnet.bai3b.mongodb.net/?retryWrites=true&w=majority&appName=taskmanagemnet", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+    JWT_SECRET:'radsfgtshyagajgsjsh'}; 
+    mongoose.set('debug', true);
+    mongoose.connect("mongodb+srv://rahulraw2002:g9qIYXGNoc3JcQJV@taskmanagemnet.bai3b.mongodb.net/?retryWrites=true&w=majority&appName=taskmanagemnet", { serverSelectionTimeoutMS: 30000
       })
       .then(() => console.log('MongoDB connected'))
       .catch(err => console.error('MongoDB connection error:', err));
